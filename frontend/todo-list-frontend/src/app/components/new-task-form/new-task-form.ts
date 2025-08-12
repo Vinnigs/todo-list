149a61eb49eb26361caa27c5918f661fca694e71
Vinicius Gomes <vinicius.gomes77@hotmail.com>
2025-08-12 16:19:36 -0300

    setup angular e marcação dos elementos

---
+++ frontend/todo-list-frontend/src/app/components/new-task-form/new-task-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-task-form',
+  imports: [],
+  templateUrl: './new-task-form.html',
+  styleUrl: './new-task-form.css'
+})
+export class NewTaskForm {
+
+}
